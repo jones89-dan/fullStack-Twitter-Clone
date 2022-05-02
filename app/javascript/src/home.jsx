1901b763e706fa5db54c623280a580fcdb3b1d2d
jones89-dan <jones.89.dan@gmail.com>
2022-05-02 13:29:49 -0700

    Started layout React component

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './home.scss';
+import Layout from './layout';
 
 const Home = props => (
-  <h1>Home page react is working</h1>
+  <Layout>
+    <h1>Home page react is working</h1>
+  </Layout>
 )
 
 document.addEventListener('DOMContentLoaded', () => {
---
+++ app/javascript/src/layout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Layout = (props) => {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="container">
+          <a class="navbar-brand" href='#'>Twitter Clone</a>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/demo">Sign In</a>
+                </li>
+              </ul>
+            </div>
+        </div>
+      </nav>
+      <div className="container py-3">
+        {props.children}
+      </div>
+      <footer className="p-3 bg-dark">
+        <div className="container">
+          <span className="me-3 text-secondary">Built by <a href="https://github.com/jones89-dan">jones89-Dan</a> with ☕ and ⚔️</span>
+        </div>
+      </footer>
+    </React.Fragment>
+  );
+}
+
+export default Layout;
