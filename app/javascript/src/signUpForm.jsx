cbc88161019169fc071a9f3d69449d2a771778d1
jones89-dan <jones.89.dan@gmail.com>
2022-05-02 21:29:39 -0700

    Adjusted the signUpForm Layout

--- app/javascript/src/signUpForm.jsx
+++ app/javascript/src/signUpForm.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 const SignUpForm = () => {
   return (
-      <div className="sign-up col-xs-4 col-xs-offset-1">
-        <form>
-          <div className="new-to-t">
-            <p><strong>New to Twitter?</strong><span> Sign Up</span></p>
-          </div>
-            <input type="text" className="form-control username" placeholder="Username"/>
-            <input type="email" className="form-control email" placeholder="Email"/>
-            <input type="password" className="form-control password" placeholder="Password"/>
-          <button id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
-        </form>
-      </div>
+    <div className="sign-up col-xs-4 col-xs-offset-1">
+      <form>
+        <div className="new-to-t">
+          <p><strong>New to Twitter?</strong><span> Sign Up</span></p>
+        </div>
+        <div className="form-group">
+          <input type="text" className="form-control username" placeholder="Username"/>
+        </div>
+        <div className="form-group">
+          <input type="email" className="form-control email" placeholder="Email"/>
+        </div>
+        <div class="form-group">
+          <input type="password" className="form-control password" placeholder="Password"/>
+        </div>
+        <button id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
+      </form>
+    </div>
   );
 }
 
