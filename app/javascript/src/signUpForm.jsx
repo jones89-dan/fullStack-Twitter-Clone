f9ba7b7cdaff75efe57cae44ffaf92ce6d1e6b25
jones89-dan <jones.89.dan@gmail.com>
2022-05-04 22:06:16 -0700

    Working on form submission using React

--- app/javascript/src/signUpForm.jsx
+++ app/javascript/src/signUpForm.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 
-const SignUpForm = () => {
+// Attempting to handle the form submission using React props
+export default class SignUpForm extends React.Component {
+  contstructor(props) {
+    super(props);
+
+    this.state = {
+      userName: "",
+      userEmail: "",
+      userPassword: "",
+    };
+  }
+
+  handeInput(event) {
+    this.setState({
+        userName: event.target.value,
+        userEmail: event.target.value,
+        userPasswor: event.target.value,
+    });
+  }
+
+
   return (
     <div className="sign-up col-xs-4 col-xs-offset-1">
       <form>
@@ -13,13 +33,45 @@ const SignUpForm = () => {
         <div className="form-group">
           <input type="email" className="form-control email" placeholder="Email"/>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input type="password" className="form-control password" placeholder="Password"/>
         </div>
         <button id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
       </form>
     </div>
   );
+
+// Create user Ajax request
+
+function createUser(username, email, password) {
+  var newRequest = new Request();
+  newRequest['type'] = 'POST';
+  newRequest['url'] = 'users';
+  newRequest['data'] = {
+    'user': {
+      'username': username,
+      'email': email,
+      'password': password
+    }
+  };
+  newRequest['success'] = function(response){
+    console.log(response);
+  };
+
+  $.ajax(newRequest);
+};
+
+// Sign up Form submission
+  $(document).on('click', '#sign-up-btn', function(e){
+    e.preventDefault();
+    var userName = $('.sign-up .username');
+    var userEmail = $('.sign-up .email.').val();
+    var userPassword = ('.sign-up .password.').val();
+
+    createUser(usernameInput, emailInput, passwordInput);
+
+  });
+
 }
 
 export default SignUpForm;
