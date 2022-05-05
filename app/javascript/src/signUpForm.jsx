3489ede42bd892e2095582b0618c5c50b6af0ac9
jones89-dan <jones.89.dan@gmail.com>
2022-05-04 22:49:09 -0700

    Working on setState for input elements

--- app/javascript/src/signUpForm.jsx
+++ app/javascript/src/signUpForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Attempting to handle the form submission using React props
 export default class SignUpForm extends React.Component {
-  contstructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -12,66 +12,58 @@ export default class SignUpForm extends React.Component {
     };
   }
 
-  handeInput(event) {
+  handleInput(event) {
     this.setState({
-        userName: event.target.value,
-        userEmail: event.target.value,
-        userPasswor: event.target.value,
+      userName: this.event.target.value,
+      userPassword: this.event.target.value,
+      userEmail: this.event.target.value,
     });
   }
 
-
-  return (
-    <div className="sign-up col-xs-4 col-xs-offset-1">
-      <form>
-        <div className="new-to-t">
-          <p><strong>New to Twitter?</strong><span> Sign Up</span></p>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control username" placeholder="Username"/>
-        </div>
-        <div className="form-group">
-          <input type="email" className="form-control email" placeholder="Email"/>
-        </div>
-        <div className="form-group">
-          <input type="password" className="form-control password" placeholder="Password"/>
-        </div>
-        <button id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
-      </form>
-    </div>
-  );
-
-// Create user Ajax request
-
-function createUser(username, email, password) {
-  var newRequest = new Request();
-  newRequest['type'] = 'POST';
-  newRequest['url'] = 'users';
-  newRequest['data'] = {
-    'user': {
-      'username': username,
-      'email': email,
-      'password': password
-    }
-  };
-  newRequest['success'] = function(response){
+  createUser(username, email, password) {
+    var newRequest = new Request();
+    newRequest['type'] = 'POST';
+    newRequest['url'] = 'users';
+    newRequest['data'] = {
+      'user': {
+        'username': username,
+        'email': email,
+        'password': password
+      }
+    };
+    newRequest['success'] = function(response){
+      console.log(response);
+    };
     console.log(response);
+    $.ajax(newRequest);
   };
 
-  $.ajax(newRequest);
-};
+  handlFormSubmission() {
+    var userName = this.state.userName;
+    var userEmail = this.state.userEmail;
+    var userPassword = this.state.userPassword;
+    createUser(userName, userEmail, userPassword);
+  }
 
-// Sign up Form submission
-  $(document).on('click', '#sign-up-btn', function(e){
-    e.preventDefault();
-    var userName = $('.sign-up .username');
-    var userEmail = $('.sign-up .email.').val();
-    var userPassword = ('.sign-up .password.').val();
-
-    createUser(usernameInput, emailInput, passwordInput);
-
-  });
-
+  render () {
+    return (
+      <div className="sign-up col-xs-4 col-xs-offset-1">
+        <form>
+          <div className="new-to-t">
+            <p><strong>New to Twitter?</strong><span> Sign Up</span></p>
+          </div>
+          <div className="form-group">
+            <input type="text" value={this.state.userName} onChange={this.handleInput} className="form-control username" placeholder="Username"/>
+          </div>
+          <div className="form-group">
+            <input type="email" value={this.state.userEmail} onChange={this.handleInput} className="form-control email" placeholder="Email"/>
+          </div>
+          <div className="form-group">
+            <input type="password" value={this.state.userPassword} onChange={this.handleInput} className="form-control password" placeholder="Password"/>
+          </div>
+          <button onClick={this.handlFormSubmission.bind(this)} id="sign-up-btn" className="btn btn-default btn-warning pull-right">Sign up for Twitter</button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default SignUpForm;
