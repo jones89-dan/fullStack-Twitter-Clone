3a261de2476689b09854f73d86a992655d6668f3
jones89-dan <jones.89.dan@gmail.com>
2022-05-28 09:43:46 -0700

    Udated model to allow image post with out message

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
   validates :user, presence: true
-  validates :message, presence: true, length: { maximum: 140 }
+  validates :message, length: { maximum: 140 }
 
   has_one_attached :image
 end
